4878dfef54f0df1127a4c900c51f6ccf44d90a59
Léo Andéol <leo.andeol@gmail.com>
2017-04-19 15:01:02 +0700

    Added session management for connection, login screen and a basic, mostly test-aimed controller

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * @Route("/user/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+	//temporary
+        $session = $request->getSession();	
+	$session->set('connected','true');
+        // replace this example code with whatever you need
+        return $this->render('user/login.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/user/loggedin", name="loggedin")
+     */
+    public function loggedInAction(Request $request)
+    {
+        // database check etc
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/user/logout", name="logout")
+     */
+    public function logoffAction(Request $request)
+    {
+	$request->getSession()->invalidate();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/user/account", name="account")
+     */
+    public function AccountAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/about.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
