92809e7b1449410d2fab70b87e6ccca7811da1ef
Léo Andéol <leo.andeol@gmail.com>
2017-04-27 14:22:07 +0700

    Adding test outputs

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,8 +15,10 @@ class DefaultController extends Controller
     {
 	$rep = $this->getDoctrine()->getManager()->getRepository('AppBundle:Item');
 	$top = $rep->findTop5PopularBooks();
-	//$last = $rep->findLast5BooksAdded();
-        // replace this example code with whatever you need
+	$last = $rep->findLast5BooksAdded();
+	var_dump($top);
+	var_dump($last);
+	// replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
