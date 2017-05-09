1e1a4d2e7d1c62c2a6fae7c9ce733246890c00d7
Lothair KIZARDJIAN <lothair.kizardjian@laposte.net>
2017-05-09 15:16:22 +0700

    fixed minor bug

--- src/AppBundle/Entity/Type.php
+++ src/AppBundle/Entity/Type.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TypeRepository")
  * @ORM\Table(name="type")
  */
 class Type{
