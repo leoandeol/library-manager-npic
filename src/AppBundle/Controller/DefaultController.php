0e9c3955a5076333b4286baa84faae16f19f6f31
Lothair KIZARDJIAN <lothair.kizardjian@laposte.net>
2017-06-13 16:26:47 +0700

    Check transaction function added needs to test it

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -80,11 +80,42 @@ class DefaultController extends Controller
     public function CheckStateOfTransactionsAction(Request $request,$password)
     {
         if($password == "20041808"){
-            //TODO
+			$mailer		= $this->get('my.mailer');
+			$em		    = $this->getDoctrine()->getManager();
+			$lib_rep  	= $em->getRepository('AppBundle:Librarian');
+			$memb_rep 	= $em->getRepository('AppBundle:Member');
+			$trans_rep	= $em->getRepository('AppBundle:Transaction');
+			$librarians = $lib_rep->findAll();
+            $members 	= $memb_rep->findAll();
 			
-            // librarians
-            $librarians = $this->getDoctrine()->getManager()->getRepository("AppBundle:Librarian")->findAll();
-            $members = $this->getDoctrine()->getManager()->getRepository("AppBundle:Member")->findAll();
+			foreach($members as $member){			
+				$today 		  = new \DateTime(date('Y-m-d'));
+				$memb_mail    = $member->getMail();
+				$transactions = $trans_rep->findBy(array('member'=>$member));
+				foreach($transactions as $transac){
+					$id 	  = $transac->getId();
+					$toReturn = $transac->getToReturnDate();
+					$state	  = $transac->getState();
+					if($today < $toReturn && $state == 'Borrowed'){
+						$dif  = $toReturn->diff($today)->days;
+						if($dif <= 3){
+							$mailer->sendTemplateMessage($memb_mail,"NPIC Library return reminder","email/reminderToReturn.html.twig",
+							array('trans_id' => $id, 'days' => $dif,'date' => $toReturn));	
+						}
+					}else if($today > $toReturn && $state == 'Borrowed'){
+						$dif  = $today->diff($toReturn)->days;
+						$fcdp = $transac->getFineCostPerDay();
+						$toPay= $dif * $fcdp;
+						$transac->setFineToPay($toPay);
+						$mailer->sendTemplateMessage($memb_mail,"NPIC Library return reminder","email/reminderToReturnAndPay.html.twig",
+						array('trans_id' => $id, 'days' => $dif,'fcpd' => $toReturn,'toPay' => $toPay));
+						foreach($librarians as $librarian){
+							$mailer->sendTemplateMessage($memb_mail,"NPIC Library transaction ended","email/reminderTransactionEnded.html.twig",
+							array('trans_id' => $id,'memb_id' => $member->getCode(), 'days' => $dif,'toPay' => $toPay));
+						}
+					}
+				}
+			}
             return new Response('OK');
         }
         else {
